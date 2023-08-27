649729021399fd3fac5a5d1697843e7adac235c9
Charith <vinodyav@tellko.com>
2023-08-27 19:54:47 +0530

    Create Company Table Fix change password issue

--- rest-service/app.js
+++ rest-service/app.js
@@ -1,6 +1,8 @@
 const AWS = require('aws-sdk')
+const { getUsers, getUserById, loginUser, addOrUpdateAdminUser, changePassword } = require('./dynamo');
 const express = require('express');
 const bodyParser = require('body-parser');
+const e = require('express');
 const app = express();
 require('dotenv').config();
 
@@ -12,45 +14,75 @@ AWS.config.update({
     secretAccessKey: process.env.AWS_SECRET_KEY_ID
 })
 
-const docClient = new AWS.DynamoDB.DocumentClient();
-const ADMIN_USERS_TABLE_NAME = "smart-attendance-admin-users";
-
 app.use(bodyParser.json());
 
 // Define a route to handle user login
 app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    const params = {
-        TableName: ADMIN_USERS_TABLE_NAME,
-    };
-
     try {
-        const result = await docClient.scan(params).promise();
-        const users = result.Items;
-
-        const user = users.find(u => u.username === username && u.password === password);
-
+        const { username, password } = req.body;
+        const user = await loginUser(username, password)
         if (user) {
             // Return a separate user object without the password field
-            const { password,username, ...userData } = user;
-            res.json({ message: 'Login successful', user: userData });
+            const { password, username, ...userData } = user;
+            res.json({ message: 'Login successful', data: userData });
         } else {
-            res.status(401).json({ error: 'Invalid credentials' });
-        }
+            res.json({ success: false, message: 'Invalid credentials' });
+                   }
     } catch (error) {
         res.status(500).json({ error: 'Error processing request' });
     }
 });
 
 app.get('/users', async (req, res) => {
-    const params = {
-        TableName: ADMIN_USERS_TABLE_NAME,
-    };
+    try {
+        // const result = await docClient.scan(params).promise();
+        const result = await getUsers()
+        // Create a new array of user objects without the password field
+        const usersWithoutPasswords = result.Items.map(user => {
+            const { password, username, ...userWithoutPassword } = user;
+            return userWithoutPassword;
+        });
+        res.json({ success: true, data: usersWithoutPasswords });
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching data from DynamoDB' });
+    }
+});
+
+app.get('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const result = await getUserById(id)
+        // Return a separate user object without the password field
+        const { password, username, ...userData } = result;
+        res.json({ success: true, data: userData });
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching data from DynamoDB' });
+    }
+});
+
+
+app.post('/updateUser', async (req, res) => {
+    try {
+        const user = req.body;
+        const result = await addOrUpdateAdminUser(user)
+        res.json({ success: true, data: result });
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching data from DynamoDB' });
+    }
+});
+
+app.put('/change-password/:id', async (req, res) => {
 
     try {
-        const result = await docClient.scan(params).promise();
-        res.json(result.Items);
+        const id = req.params.id;
+        const { oldPassword, newPassword } = req.body;
+        const result = await changePassword(id, oldPassword, newPassword)
+        
+        if (result) {
+            res.json({ success: true, message: "Password changed successfully" });
+        } else {
+            res.json({ success: false, message: result });
+        }
     } catch (error) {
         res.status(500).json({ error: 'Error fetching data from DynamoDB' });
     }
--- rest-service/dynamo.js
+++ rest-service/dynamo.js
@@ -1,6 +1,9 @@
 const AWS = require('aws-sdk')
 require('dotenv').config();
 
+ADMIN_USERS_TABLE_NAME = process.env.ADMIN_USERS_TABLE_NAME
+COMPANY_TABLE_NAME = process.env.COMPANY_TABLE_NAME
+
 AWS.config.update({
     region: process.env.AWS_DEFAULT_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -8,7 +11,6 @@ AWS.config.update({
 })
 
 const docClient = new AWS.DynamoDB.DocumentClient();
-const ADMIN_USERS_TABLE_NAME = "smart-attendance-admin-users";
 
 const getUsers = async () => {
     const params = {
@@ -25,17 +27,83 @@ const getUsers = async () => {
     return result
 }
 
-async function addAdminUser(user) {
+const getUserById = async (id) => {
+    const params = {
+        TableName: ADMIN_USERS_TABLE_NAME,
+        Key: {
+            id
+        }
+    };
+    try {
+        const result = await docClient.get(params).promise();
+        return result.Item;
+      } catch (error) {
+        console.error('Error getting user by ID:', error);
+      }
+}
+
+const loginUser = async (username, password) => {
+
+    const params = {
+        TableName: ADMIN_USERS_TABLE_NAME,
+    };
+    const result = await docClient.scan(params).promise();
+    const users = result.Items;
+
+    var loggedInUser=users.find(u => u.username === username && u.password === password);
+    return loggedInUser
+}
+
+async function addOrUpdateAdminUser(user) {
     const params = {
         TableName: ADMIN_USERS_TABLE_NAME,
         Item: user
     };
+    return await docClient.put(params).promise();
+}
+
+const changePassword = async (id, oldPassword, newPassword) => {
     try {
-        const result = await docClient.put(params).promise();
-        console.log('Scan result:', result.Items);
+        const user =await getUserById(id);
+
+        if (!user) {
+            return 'User not found';
+        }
+
+        console.log(user)
+        if (user.password != oldPassword) {
+            return 'Invalid old password';
+        }
+
+        //Update password in DynamoDB
+        const updateParams = {
+            TableName: ADMIN_USERS_TABLE_NAME,
+            Key: { id },
+            UpdateExpression: 'set password = :password',
+            ExpressionAttributeValues: {
+                ':password': newPassword
+            },
+            // ReturnValues: 'UPDATED_NEW'
+        };
+
+        await docClient.update(updateParams).promise();
+        return true
+
     } catch (error) {
-        console.error('Error scanning table:', error);
+        return error;
     }
+
+}
+
+//User Table
+async function addOrUpdateCompany(company) {
+    const params = {
+        TableName: COMPANY_TABLE_NAME,
+        Item: company
+    };
+
+    var result=await docClient.put(params).promise();
+    return result
 }
 
 const adminUser = {
@@ -49,9 +117,24 @@ const adminUser = {
     "lastName": "Vithanage"
 }
 
+const employeeCompany = {
+    "id": "1",
+    "employeeID": "SHDL00001",
+    "companyName": "ABC Company"
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    loginUser,
+    addOrUpdateAdminUser,
+    changePassword
+}
+
+addOrUpdateCompany(employeeCompany)
 // addAdminUser(adminUser)
 
-getUsers();
+// getUsers();
 
 
 
